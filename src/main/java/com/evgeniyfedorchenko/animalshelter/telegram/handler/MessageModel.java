5e5add469506282ad80e9e6b3ef3d602df6ed9e9
Evgeniy Fedorchenko <jecky432@gmail.com>
2024-06-24 23:44:40 +0500

    few changes in javadoc of some telegram's classes

--- src/main/java/com/evgeniyfedorchenko/animalshelter/telegram/handler/MessageModel.java
+++ src/main/java/com/evgeniyfedorchenko/animalshelter/telegram/handler/MessageModel.java
@@ -5,35 +5,38 @@ import jakarta.annotation.Nullable;
 import lombok.Builder;
 import lombok.Getter;
 import org.telegram.telegrambots.meta.api.methods.send.SendMessage;
+import org.telegram.telegrambots.meta.api.methods.updatingmessages.EditMessageText;
 import org.telegram.telegrambots.meta.api.objects.Update;
 
 import java.util.Map;
 
 /**
  * Класс представляет собой исходную модель для накопления всех необходимых параметров
- * и формирования объекта {@link SendMessage}, который может быть отправлен напрямую через Telegram
+ * и формирования объекта {@link SendMessage} или {@link EditMessageText}, который может
+ * быть отправлен напрямую через Telegram
  *
- * @apiNote Чтобы преобразовать модель в объект сообщения {@code SendMessage},
+ * @apiNote Чтобы преобразовать модель в объект сообщения {@code SendMessage} или {@code EditMessageText},
  * используйте {@link MessageUtils#applySimpled(MessageModel)} или {@link MessageUtils#applyCallback(MessageModel)}
+ * соответственно
  */
 @Getter
 @Builder
 public class MessageModel {
 
     /**
-     * Идентификатор чата, для которого предназначено будущее сообщение
+     * Идентификатор чата, для которого предназначено сообщение
      */
     private final String chatId;
 
     /**
-     * Идентификатор сообщения, для которого предназначено будущее сообщение.
+     * Идентификатор сообщения, которое будет изменено на новое.
      * Актуально для имплементаций {@link Callback}
      */
     @Nullable
     private final Integer messageId;
 
     /**
-     * Объект класса enumeration, из которого будет взят текст сообщения для последующей отправки
+     * Объект enumeration, из которого будет взят текст сообщения для последующей отправки
      */
     private final MessageData messageData;
 
@@ -41,7 +44,7 @@ public class MessageModel {
      * Карта, которая будет преобразована в объект клавиатуры для отправляемого сообщения.<br>
      * <b>Key</b> - Текст, который будет отображаться на кнопке<br>
      * <b>Value</b> - Строковый идентификатор вызова, который может быть перехвачен в {@link MainHandler#handleCallbacks(Update)}.
-     * Оно должно совпадать с именем компонента источника, который отправляет эту кнопку
+     * Он должен совпадать с именем компонента источника, который отправляет эту кнопку
      */
     @Nullable
     private final Map<String, String> keyboardData;
